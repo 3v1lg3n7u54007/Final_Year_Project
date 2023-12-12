942bf7b27bcf422ae7e87b01022570bc0738cdde
3v1lg3n7u54007 <3v1lg3n7u54007@gmail.com>
2023-12-12 01:56:28 -0500

    Added a feature to let the user select scan type [Network based or HOst based]

--- RDP_X/rdp_scanner.sh
+++ RDP_X/rdp_scanner.sh
@@ -1,22 +1,5 @@
 #!/bin/bash
 
-echo "--[ TOOL ]--[ RDP SCANNER ]--[ SUJAL TULADHAR ]"
-
-# Prompt the user for the network range using Zenity
-network_range=$(zenity --entry --title="Network Scanner" --text="Enter the network range (e.g., 192.168.15.0):")
-
-# Validate the input
-if [[ ! $network_range =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
-    zenity --error --text="Invalid network range format. Please enter a valid IP address."
-    exit 1
-fi
-
-# Specify the RDP port
-rdp_port=3389
-
-# Extract the network prefix (e.g., 192.168.15)
-network_prefix="${network_range%.*}"
-
 # Function to check RDP port for an individual host
 check_rdp_port() {
     local host=$1
@@ -26,28 +9,71 @@ check_rdp_port() {
     fi
 }
 
-# Function to display scanning message
-display_scanning_message() {
-    echo "Scanning the network..."
-}
+echo "--[ TOOL ]--[ RDP SCANNER ]--[ SUJAL TULADHAR ]"
 
-echo "" > activeRDPs.txt
+# Prompt the user for subnet (N) or single IP (H)
+choice=$(zenity --list --title="Scan Type" --column="Type" "Network" "Host" --text="Please specify the scan type you want to perform. [Network or Host based]")
 
-# Run the scanning message display
-display_scanning_message
+if [[ $choice == "N" ]]; then
+    # Prompt the user for the network range using Zenity
+    network_range=$(zenity --entry --title="Network Scanner" --text="Enter the network range (e.g., 192.168.15.0):")
 
-# Check RDP port for each host in the specified range in the background
-for host in "$network_prefix".{1..254}; do
-    check_rdp_port "$host" &
-done
+    # Validate the input
+    if [[ ! $network_range =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        zenity --error --text="Invalid network range format. Please enter a valid IP address."
+        exit 1
+    fi
 
-# Sleep for 60 seconds
-sleep 30
+    # Specify the RDP port
+    rdp_port=3389
 
-# End the program
-echo "Scanning complete."
+    # Extract the network prefix (e.g., 192.168.15)
+    network_prefix="${network_range%.*}"
 
-# Invoke rdp_bruteforce.sh script
-./rdp_bruteforce.sh
+    # Function to display scanning message
+    display_scanning_message() {
+        echo "Scanning the network..."
+    }
 
-exit 0
+    echo "" > activeRDPs.txt
+
+    # Run the scanning message display
+    display_scanning_message
+
+    # Check RDP port for each host in the specified range in the background
+    for host in "$network_prefix".{1..254}; do
+        check_rdp_port "$host" &
+    done
+
+    # Sleep for 30 seconds
+    sleep 30
+
+    # End the program
+    echo "Scanning complete."
+    
+    ./rdp_bruteforce.sh
+
+elif [[ $choice == "H" ]]; then
+    # Prompt the user for a single IP address
+    host_ip=$(zenity --entry --title="Host Scanner" --text="Enter the host IP address:")
+    
+    # Validate the input
+    if [[ ! $host_ip =~ ^[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+$ ]]; then
+        zenity --error --text="Invalid IP address format. Please enter a valid IP address."
+        exit 1
+    fi
+
+    # Check RDP port for the specified host
+    check_rdp_port "$host_ip"
+
+    # Rewrite the content of activeRDPs.txt
+    echo "$host_ip" > activeRDPs.txt
+
+    # Invoke rdp_bruteforce.sh script
+    ./rdp_bruteforce.sh
+
+    exit 0
+else
+    zenity --error --text="Invalid choice. Please select either 'N' or 'H'."
+    exit 1
+fi
